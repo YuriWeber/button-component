a71d86eaf9deb5dbe6709fe965855589a7ecbac1
Yuri Weber <a.yuriprof@gmail.com>
2022-03-10 13:23:07 -0300

    button-component

---
+++ button-component/icons/icons.js
@@ -0,0 +1,9 @@
+import check from "./check.svg";
+import refresh from "./refresh.svg";
+import arrowRight from "./arrow-right.svg"
+
+export const Icons = {
+    "check": check,
+    "refresh": refresh,
+    "arrow-right": arrowRight
+}
